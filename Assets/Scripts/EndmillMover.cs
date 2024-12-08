234ddbf8bd8a2b4a82bb19e4631753d3cad6ed93
fish_meat <fishmeat37820@gmail.com>
2024-12-08 14:54:22 +0900

    add text file reader

--- Assets/Scripts/EndmillMover.cs
+++ Assets/Scripts/EndmillMover.cs
@@ -1,19 +1,34 @@
 using UnityEngine;
+using System.IO;
 
 public class EndmillMover : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update
+    // after the MonoBehaviour is created
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
         Debug.Log("Hello World");
+
+        // Read the gcode/test.cnc file line by line and log each line
+        string path = "Assets/gcode/test.cnc";
+        if (File.Exists(path))
+        {
+            string[] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                Debug.Log(line);
+            }
+        }
+        else
+        {
+            Debug.LogError("File not found: " + path);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(
-            transform.position, new Vector3(1, 0, 0), Time.deltaTime);
-        Debug.Log($"Current position: {transform.position}");
+
     }
 }
